247a133f4d384b174bf0346cdf5eb925dc0c5d54
Moskitol89 <kurganovv@yahoo.com>
2020-12-19 19:04:19 +0300

    index.html first slide done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
- 
+    let indexMainSlider = new Swiper(".main-slider__container", {
+        direction: "vertical",
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 });
