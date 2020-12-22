e669cc4bcc01d675c3acf032d4384f74aeb8f0dc
Moskitol89 <kurganovv@yahoo.com>
2020-12-22 22:35:52 +0300

    index.html second slide without animations

--- app/js/main.js
+++ app/js/main.js
@@ -14,21 +14,8 @@ $(function(){
             slideChangeTransitionStart: function () {
                 document.querySelector(".main-slider__slide.swiper-slide-active")
                     .classList.add("activated");
-                currentLink();
             }
         }
     });
-//функия для изменения цвета активной ссылки в панеле навигации
-    function currentLink() {
-        let currentActiveSlide = indexMainSlider.realIndex;
-        let allLinks =  document.querySelectorAll(".header__link");
-        for(let i = 0; i < allLinks.length; i++) {
-            if(i != currentActiveSlide) {
-                allLinks[i].classList.remove("active");
-            } else {
-                allLinks[i].classList.add("active");
-            }
-        }
-    }
 
 });
