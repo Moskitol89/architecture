11bb15c0be72580ff457d1892186792f8dc7cfe4
Moskitol89 <kurganovv@yahoo.com>
2021-02-18 18:14:57 +0300

    header adaptive done

--- app/js/main.js
+++ app/js/main.js
@@ -110,8 +110,10 @@ document.querySelector(".header__btn-menu").addEventListener("click", function (
     if (this.classList.contains("clicked")) {
         this.classList.remove("clicked");
         document.querySelector(".header__nav").classList.remove("show");
+        document.querySelector("header").classList.remove("btn-clicked");
     } else {
         this.classList.add("clicked");
+        document.querySelector("header").classList.add("btn-clicked");
         document.querySelector(".header__nav").classList.add("show");
     }
 });
