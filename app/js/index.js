426256e97817d4d8d9762a0b5e59afee60bd8e90
Moskitol89 <kurganovv@yahoo.com>
2021-03-22 20:00:26 +0300

    about.html adaptive done

--- app/js/index.js
+++ app/js/index.js
@@ -127,18 +127,7 @@ function countAnimation(htmlElement, maxNumber) {
     }, 100);
 }
 
-//menu button animation
-document.querySelector(".header__btn-menu").addEventListener("click", function () {
-    if (this.classList.contains("clicked")) {
-        this.classList.remove("clicked");
-        document.querySelector(".header__nav").classList.remove("show");
-        document.querySelector("header").classList.remove("btn-clicked");
-    } else {
-        this.classList.add("clicked");
-        document.querySelector("header").classList.add("btn-clicked");
-        document.querySelector(".header__nav").classList.add("show");
-    }
-});
+
 
 // add active class for current link in header (0 - index.html)
 document.querySelectorAll(".header__link")[0].classList.add("active");
